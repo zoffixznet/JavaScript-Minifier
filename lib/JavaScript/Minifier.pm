b00d70ea656fb4a544c786683516b9b3f5138490
vti <viacheslav.t@gmail.com>
2015-03-19 18:44:26 +0200

    document automatic minify exportation

--- lib/JavaScript/Minifier.pm
+++ lib/JavaScript/Minifier.pm
@@ -367,12 +367,14 @@ JavaScript::Minifier - Perl extension for minifying JavaScript code
 
 To minify a JavaScript file and have the output written directly to another file
 
-  use JavaScript::Minifier qw(minify);
+  use JavaScript::Minifier;
   open(INFILE, 'myScript.js') or die;
   open(OUTFILE, '>myScript-min.js') or die;
   minify(input => *INFILE, outfile => *OUTFILE);
   close(INFILE);
   close(OUTFILE);
+
+Function C<minify> is automatically exported.
 
 To minify a JavaScript string literal. Note that by omitting the outfile parameter a the minified code is returned as a string.
 
